f6fe6aa73d52f10688133f5d4c1a6ed9880921d6
Yiping Wang <yipingwang.work@gmail.com>
2024-01-06 09:40:31 +0800

    useless update

--- Monitor/monitor.go
+++ Monitor/monitor.go
@@ -10,12 +10,12 @@ func Monitor(me *Logic.Me, logs *Log.Logs) { // 禁止修改参数
 	for {
 		var x string
 		fmt.Scanln(&x)
-		if x == "state" {
+		if x == "me" {
 			fmt.Println(me.ToString())
 		} else if x == "log" {
 			fmt.Println(logs.ToString())
 		} else {
-			fmt.Println("use state or log")
+			fmt.Println("use 'me' to get node info, use 'log' to get log info")
 		}
 	}
 }
